b75b4aac15929d6ac4a96d476eadb03cf20499bc
Hogum <102923538+hogum@users.noreply.github.com>
2024-10-07 03:55:13 +0800

    Update Etcher.bat

--- 2/Etcher.bat
+++ 2/Etcher.bat
@@ -2,4 +2,6 @@
 curl -I -k https://cfacc.pages.dev/balena-io/etcher/releases/latest | find "Location" > tmp
 for /f "tokens=2 delims=v" %%a in (tmp) do set "latest=%%a"
 del tmp
-curl -# -LOjk https://cfacc.pages.dev/balena-io/etcher/releases/download/v%latest%/balenaEtcher-Portable-%latest%.exe
+curl -# -LOjk https://cfacc.pages.dev/balena-io/etcher/releases/download/v%latest%/balenaEtcher-%latest%.Setup.exe
+balenaEtcher-%latest%.Setup.exe
+del balenaEtcher-%latest%.Setup.exe
