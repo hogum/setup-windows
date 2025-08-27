c9fe565d23829bde15a1e81afbccf92da9291ac6
Hogum <102923538+hogum@users.noreply.github.com>
2025-08-27 17:53:51 +0800

    Add files via upload

--- 0/Drivers/NVIDIA.bat
+++ 0/Drivers/NVIDIA.bat
@@ -2,6 +2,6 @@
 curl https://www.nvidia.cn/software/nvidia-app/ | find "NVIDIA_app" > tmp
 for /f "tokens=5 delims=/" %%a in (tmp) do set "latest=%%a"
 del tmp
-curl -# -OL https://cn.download.nvidia.com/nvapp/client/%latest%/NVIDIA_app_v%latest%.exe
+curl -# -H "Referer: https://www.nvidia.cn/" -O https://cn.download.nvidia.cn/nvapp/client/%latest%/NVIDIA_app_v%latest%.exe
 NVIDIA_app_v%latest%.exe
 del NVIDIA_app_v%latest%.exe
