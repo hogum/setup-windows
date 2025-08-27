06406b3fd82292c96814c9f65504ce3edf639190
Hogum <102923538+hogum@users.noreply.github.com>
2025-08-27 17:31:46 +0800

    Add files via upload

--- 0/Drivers/Presonus.bat
+++ 0/Drivers/Presonus.bat
@@ -1,13 +1,9 @@
 @echo off
-curl https://cfacc.pages.dev/products/Universal-Control/downloads | find "exe" > tmp
-for /f tokens^=2^ delims^=^" %%a in (tmp) do (
-	if defined stop (
-		del tmp
-		goto go
-	) else set "url=https:%%a"&set stop=1
-)
-:go
-for /f "tokens=6 delims=/" %%a in ("%url%") do set "latest=%%a"
-curl -# -O https://cfacc.pages.dev/downloads/products/exe/%latest%
+chcp 65001
+curl -H "X-Requested-With: XMLHttpRequest" -d "id=43" https://www.acproaudio.com/index/choose/showfile.html > tmp
+for /f "tokens=2 delims=}" %%a in (tmp) do set "latest=%%a"
+for /f "tokens=4 delims=/" %%a in ("%latest%") do set "latest=%%~na"
+del tmp
+curl -# -O https://www.fmicassets.com/Damroot/Original/10111/%latest%
 %latest%
 del %latest%
