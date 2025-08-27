34d954a699b40c33674ccd0452657d8db591c28f
Hogum <102923538+hogum@users.noreply.github.com>
2025-08-27 18:39:07 +0800

    Add files via upload

--- 2/Ventoy.bat
+++ 2/Ventoy.bat
@@ -1,7 +1,4 @@
 @echo off
-curl -I -k https://cfacc.pages.dev/ventoy/Ventoy/releases/latest | find "Location" > tmp
-for /f "tokens=3 delims=v" %%a in (tmp) do set "latest=%%a"
-del tmp
-curl -# -LOjk https://cfacc.pages.dev/ventoy/Ventoy/releases/download/v%latest%/ventoy-%latest%-windows.zip
-powershell Expand-Archive -Path ventoy-%latest%-windows.zip -DestinationPath %~dp0 -Force
-del ventoy-%latest%-windows.zip
+curl -# -o ventoy.zip https://acc.189168.xyz/ventoy
+powershell Expand-Archive -Path ventoy.zip -DestinationPath %~dp0 -Force
+del ventoy.zip
