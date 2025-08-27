aede1598aeaef7ba747cd95ada7f28d63f9d5f6c
Hogum <102923538+hogum@users.noreply.github.com>
2025-08-27 18:35:07 +0800

    Delete 2/OBS.bat

--- 2/OBS.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-curl https://obsproject.com/download | findstr /r "downloads/OBS-Studio-[0-9.]*-Windows-Installer.exe" > tmp
-for /f tokens^=2^ delims^=^" %%a in (tmp) do set "url=%%a"
-for /f "tokens=4 delims=/" %%a in ("%url%") do set "latest=%%a"
-del tmp
-curl -# -O https://cfacc.pages.dev/downloads/%latest%
-%latest%
-del %latest%
