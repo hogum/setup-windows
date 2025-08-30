906e36624f5b4eab6dd66ea46ad630c3a9986445
Hogum <102923538+hogum@users.noreply.github.com>
2025-08-30 16:30:03 +0800

    Add files via upload

--- 0/CentBrowser.bat
+++ 0/CentBrowser.bat
@@ -2,6 +2,13 @@
 curl https://centbrowser.cn/ | find "x64.exe" > tmp
 for /f "tokens=4 delims=/" %%a in (tmp) do set "latest=%%a"
 del tmp
-curl -# -O https://static.centbrowser.cn/win_stable/%latest%/centbrowser_%latest%_x64.exe
+set URL=https://static.centbrowser.cn/win_stable/%latest%/centbrowser_%latest%_x64.exe
+for /f %%i in ('curl --head -s -o nul -w "%%{http_code}" %URL%') do (
+    if %%i==200 (
+        curl -# -O https://static.centbrowser.cn/win_stable/%latest%/centbrowser_%latest%_x64.exe
+    ) else (
+        curl -# -O https://static.centbrowser.com/win_stable/%latest%/centbrowser_%latest%_x64.exe
+    )
+)
 centbrowser_%latest%_x64.exe
 del centbrowser_%latest%_x64.exe
