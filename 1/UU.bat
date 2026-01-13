c3cf89697bef985d0e1876b1d6a952984005ec54
Hogum <102923538+hogum@users.noreply.github.com>
2026-01-13 23:26:25 +0800

    Add files via upload

--- 1/UU.bat
+++ 1/UU.bat
@@ -3,6 +3,6 @@ curl https://adl.netease.com/d/g/uu/c/gw?type=pc | findstr /r "UU-[0-9.]*.exe" >
 for /f tokens^=2^ delims^=^" %%a in (tmp) do set "url=%%a"
 for /f "tokens=4 delims=/?" %%a in ("%url%") do set "installer=%%a"
 del tmp
-curl -# -O %url%
+curl -# -O "%url%"
 %installer%
 del %installer%
