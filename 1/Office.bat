1c325e900ed219c385b4d89b5e25f6c2aec6817d
Hogum <102923538+hogum@users.noreply.github.com>
2026-02-04 02:23:22 +0800

    Add files via upload

--- 1/Office.bat
+++ 1/Office.bat
@@ -1,4 +1,4 @@
 @echo off
-curl -# -o officetool.zip https://acc.189168.xyz/officetool
+curl -#L -o officetool.zip https://otp.landian.vip/redirect/download.php?type=runtime&arch=x64
 powershell Expand-Archive -Path officetool.zip -DestinationPath %~dp0 -Force
 del officetool.zip
